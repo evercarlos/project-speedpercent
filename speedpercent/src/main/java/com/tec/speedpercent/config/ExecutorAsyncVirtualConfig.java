e31b3af6059c462221ff1a9e0e75b972de5aaebb
evercarlos <ecarlosr@alumno.unsm.edu.pe>
2025-04-30 19:49:55 -0500

    update executor

--- speedpercent/src/main/java/com/tec/speedpercent/config/ExecutorAsyncVirtualConfig.java
+++ speedpercent/src/main/java/com/tec/speedpercent/config/ExecutorAsyncVirtualConfig.java
@@ -6,23 +6,14 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
 
 @Configuration
 public class ExecutorAsyncVirtualConfig {
 
     @Bean(name = BeanConstants.ASYNC_VIRTUAL_SAVE_CALL_HISTORY)
     public Executor asyncVirtualSaveCallHistory() {
-        return Executors.newThreadPerTaskExecutor(new ThreadFactory() {
-            private final ThreadFactory defaultFactory = Executors.defaultThreadFactory();
-            private int counter = 0;
-
-            @Override
-            public Thread newThread(Runnable r) {
-                Thread thread = defaultFactory.newThread(r);
-                thread.setName(STR."virtual-thread-\{counter++}");
-                return thread;
-            }
-        });
+        return Executors.newThreadPerTaskExecutor(
+                Thread.ofVirtual().name("virtual-thread-", 0)::unstarted
+        );
     }
 }
